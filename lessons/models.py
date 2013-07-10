ec0be6eb0b71bd088ea008fa87ca2101226a5ed9
Alex J Best <alex.j.best@gmail.com>
2013-07-10 12:12:40 +0000

    playing with models

--- lessons/models.py
+++ lessons/models.py
@@ -4,8 +4,12 @@ from django.db import models
 class Lesson(models.Model):
     name = models.CharField(max_length = 100)
     tutorial = models.CharField(max_length = 1000)
+    def __unicode__(self):
+        return self.name
 
 class Question(models.Model):
     lesson = models.ForeignKey(Lesson)
     text = models.CharField(max_length = 300)
     answer = models.CharField(max_length = 200)
+    def __unicode__(self):
+        return self.text
