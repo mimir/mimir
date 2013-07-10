06324c23c65dae4eaa5d2b1d7aeeeecd372a814b
Alex J Best <alex.j.best@gmail.com>
2013-07-10 13:58:42 +0000

    fun with templates

--- lessons/views.py
+++ lessons/views.py
@@ -1,7 +1,15 @@
 from django.http import HttpResponse
+from django.template import RequestContext, loader
+
+from lessons.models import Lesson
 
 def index(request):
-    return HttpResponse("sup")
+    latest_lesson_list = Lesson.objects.all()[:5]
+    template = loader.get_template('lessons/index.html')
+    context = RequestContext(request, {
+        'latest_lesson_list':latest_lesson_list,
+    })
+    return HttpResponse(template.render(context))
 
 def read(request, lesson_id):
     return HttpResponse("You are looking at lesson %s." % lesson_id)
