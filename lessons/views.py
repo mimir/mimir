09020f2d4c6a5e786f204b59b53609412689b530
Alex J Best <alex.j.best@gmail.com>
2013-07-10 14:28:40 +0000

    fun with urls

--- lessons/views.py
+++ lessons/views.py
@@ -1,18 +1,22 @@
-from django.http import HttpResponse
-from django.template import RequestContext, loader
+from django.shortcuts import render
+from django.http import HttpResponse, Http404
 
 from lessons.models import Lesson
 
 def index(request):
     latest_lesson_list = Lesson.objects.all()[:5]
-    template = loader.get_template('lessons/index.html')
-    context = RequestContext(request, {
+    context = ({
         'latest_lesson_list':latest_lesson_list,
     })
-    return HttpResponse(template.render(context))
+    return render(request, 'lessons/index.html', context)
 
 def read(request, lesson_id):
-    return HttpResponse("You are looking at lesson %s." % lesson_id)
+    try:
+        lesson = Lesson.objects.get(pk=lesson_id)
+        context = ({'lesson': lesson,})
+    except Lesson.DoesNotExist:
+        raise Http404
+    return render(request, 'lessons/read.html', context)
 
 def question(request, lesson_id):
     return HttpResponse("You are looking at a question for lesson %s." % lesson_id)
