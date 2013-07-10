33c26645bd403359e4abf1659fb82e53892a234b
Alex J Best <alex.j.best@gmail.com>
2013-07-10 12:06:08 +0000

    changed naming

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Mimir.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mimir.settings")
 
     from django.core.management import execute_from_command_line
 
