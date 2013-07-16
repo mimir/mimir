ad707fc1b6f7a3ba0adb8eac4c305f286ccf442b
Alex J Best <alex.j.best@gmail.com>
2013-07-16 22:18:10 +0000

    small changes

--- mimir/views.py
+++ mimir/views.py
@@ -2,6 +2,7 @@ from django import forms
 from django.shortcuts import render
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
 
 def index(request):
     return render(request, 'home.html')
@@ -11,6 +12,10 @@ def register(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             new_user = form.save()
+            username = request.POST['username']
+            password = request.POST['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
             return HttpResponseRedirect("/home/")
     else:
         form = UserCreationForm()
