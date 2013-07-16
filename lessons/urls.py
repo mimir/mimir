097b4ea94de5ac6ed46a71dce8f275001babb43a
Alex J Best <alex.j.best@gmail.com>
2013-07-16 20:05:54 +0000

    spacing

--- lessons/urls.py
+++ lessons/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import patterns, url
 from lessons import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name  = 'index'),
-    url(r'^(?P<lesson_id>\d+)/$', views.read, name  = 'read'),
-    url(r'^(?P<lesson_id>\d+)/question/$', views.question, name  = 'question'),
+    url(r'^$', views.index, name = 'index'),
+    url(r'^(?P<lesson_id>\d+)/$', views.read, name = 'read'),
+    url(r'^(?P<lesson_id>\d+)/question/$', views.question, name = 'question'),
 
 )
