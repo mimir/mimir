f93f6ae03332a6d6ad655558e65a3ac8af254164
Alex J Best <alex.j.best@gmail.com>
2013-07-18 23:57:04 +0000

    started to work on questions framework

--- lessons/urls.py
+++ lessons/urls.py
@@ -4,6 +4,6 @@ from lessons import views
 urlpatterns = patterns('',
     url(r'^$', views.index, name = 'index'),
     url(r'^(?P<lesson_id>\d+)/$', views.read, name = 'read'),
-    url(r'^(?P<lesson_id>\d+)/question/$', views.question, name = 'question'),
+    url(r'^(?P<lesson_id>\d+)/test/(?P<question_id>\d+)/$', views.question, name = 'question'),
 
 )
--- lessons/views.py
+++ lessons/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, Http404
 
-from lessons.models import Lesson, Example
+from lessons.models import Lesson, Example, Question
 
 def index(request):
     latest_lesson_list = Lesson.objects.all()[:5]
@@ -14,11 +14,16 @@ def read(request, lesson_id):
     try:
         lesson = Lesson.objects.get(pk = lesson_id)
         example_list = Example.objects.filter(lesson__id = lesson_id)
-        context = ({'lesson': lesson,'example_list':example_list,})
+        first_question = Question.objects.filter(lesson__id = lesson_id)[0]
+        context = ({'lesson': lesson,'example_list':example_list,'first_question':first_question,})
     except Lesson.DoesNotExist:
         raise Http404
     return render(request, 'lessons/read.html', context)
 
-def question(request, lesson_id):
-    return HttpResponse("You are looking at a question for lesson %s." % lesson_id)
-
+def question(request, lesson_id, question_id):
+    try:
+        question = Question.objects.get(pk = question_id)
+        context = ({'question': question,})
+    except Question.DoesNotExist:
+        raise Http404
+    return render(request, 'lessons/question.html', context)
