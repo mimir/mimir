e892501b3061eea90a5383d4eb9f451ad5b64b9e
Alex J Best <alex.j.best@gmail.com>
2013-07-19 23:50:14 +0000

    add extra to admin panel

--- user_profiles/admin.py
+++ user_profiles/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from user_profiles.models import UserProfile
+from user_profiles.models import UserProfile, UserTakesLesson, UserAnswersQuestion
 
 admin.site.register(UserProfile)
+admin.site.register(UserAnswersQuestion)
+admin.site.register(UserTakesLesson)
