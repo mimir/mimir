7a0d85c6474a717ecc6e107be19a5eb625b3c3a4
Alex J Best <alex.j.best@gmail.com>
2013-07-20 13:56:24 +0000

    add lesson links to admin

--- lessons/admin.py
+++ lessons/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from lessons.models import Lesson, Question, Example, AnswerFormat
+from lessons.models import Lesson, Question, Example, AnswerFormat, LessonFollowsFromLesson
 
 admin.site.register(Lesson)
 admin.site.register(Question)
 admin.site.register(Example)
 admin.site.register(AnswerFormat)
+admin.site.register(LessonFollowsFromLesson)
