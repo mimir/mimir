fbce6244ad2ac3489d25df7681d1d979232c12b0
Alex J Best <alex.j.best@gmail.com>
2013-07-20 13:51:39 +0000

    add queries for times taken and times answered to questions and lessons

--- lessons/models.py
+++ lessons/models.py
@@ -7,8 +7,11 @@ class Lesson(models.Model):
     tutorial = models.TextField()
     created = models.DateTimeField(auto_now_add = True) #Creation date set on adding
     modified = models.DateTimeField(auto_now = True) #Modification date set on changing
-    times_taken = models.IntegerField(default = 0) #TODO make this a cache for query on usertakeslessons
     description = models.CharField(max_length = 400)
+    @property
+    def times_taken(self):
+        return self.usertakeslesson_set.count() #TODO ensure this is efficient
+
     def __unicode__(self):
         return self.name
 
@@ -37,12 +40,17 @@ class Question(models.Model):
     calculation = models.TextField() #TODO work out how on earth this will work
     created = models.DateTimeField(auto_now_add = True) #Creation date set on adding
     modified = models.DateTimeField(auto_now = True) #Modification date set on changing
-    times_answered = models.IntegerField(default = 0) #TODO make this cache of query as above
+    @property
+    def times_answered(self):
+        return self.useranswersquestion_set.count() #TODO ensure this is efficient
+    #TODO add other useful queries here e.g. times answered correctly
     def __unicode__(self):
         return self.question
 
 class LessonFollowsFromLesson(models.Model):
-    leads_from = models.ForeignKey(Lesson)
-    leads_to = models.ForeignKey(Lesson)
+    leads_from = models.ForeignKey(Lesson, related_name='prerequisite')
+    leads_to = models.ForeignKey(Lesson, related_name='postrequisite') #TODO think about these two related names, do they make sense?
     CHOICES = [(i,i) for i in range(11)] #0-10
     strength = models.PositiveSmallIntegerField(choices = CHOICES) #Not sure exactly how this will work yet TODO work it out. Roughly 10 should correspond to, you really need to know A to do B, and 0 should mean that they're sort of linked?
+    def __unicode__(self):
+        return self.leads_from.name + " leads to " + self.leads_to.name
