a2fe7cf5f1b6c6e7f942cb184a1e7d56a5220990
Alex J Best <alex.j.best@gmail.com>
2013-07-20 01:01:18 +0000

    current progress on userprofile views, register via this view is currently very broken

--- user_profiles/views.py
+++ user_profiles/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse, Http404
 
 from user_profiles.models import UserProfile
+from user_profiles.forms import UserProfileForm
 from django.contrib.auth.models import User
 
 def index(request):
-    user_list = UserProfile.objects.all()
+    user_list = User.objects.all()
     context = ({
         'user_list':user_list,
     })
@@ -13,8 +14,25 @@ def index(request):
 
 def view(request, user_id):
     try:
-        user = UserProfile.objects.get(user__id = user_id)
-        context = ({'user': user,})
+        cur_user = User.objects.get(id = user_id)
+        cur_user_p = UserProfile.objects.get(user__id = user_id)
+        context = ({'cur_user': cur_user, 'cur_user_p': cur_user_p,})
     except UserProfile.DoesNotExist:
         raise Http404
     return render(request, 'user_profiles/view.html', context)
+
+def register(request):
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST)
+        if form.is_valid():
+            new_user = form.save()
+            username = request.POST['username']
+            password = request.POST['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return HttpResponseRedirect("/home/")
+    else:
+        form = UserProfileForm()
+    return render(request, "user_profiles/register.html", {
+        'form': form,
+    })
