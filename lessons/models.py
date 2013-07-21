1c47de761af2f2e99ebfe8ae70bee6b5b19948c8
Alex J Best <alex.j.best@gmail.com>
2013-07-21 11:57:44 +0000

    add to model slightly, still need to check if the related names are cool

--- lessons/models.py
+++ lessons/models.py
@@ -48,9 +48,13 @@ class Question(models.Model):
         return self.question
 
 class LessonFollowsFromLesson(models.Model):
-    leads_from = models.ForeignKey(Lesson, related_name='prerequisite')
-    leads_to = models.ForeignKey(Lesson, related_name='postrequisite') #TODO think about these two related names, do they make sense?
+    leads_from = models.ForeignKey(Lesson, related_name='preparation')
+    leads_to = models.ForeignKey(Lesson, related_name='supplement') #TODO think about these two related names, do they make sense?
     CHOICES = [(i,i) for i in range(11)] #0-10
     strength = models.PositiveSmallIntegerField(choices = CHOICES) #Not sure exactly how this will work yet TODO work it out. Roughly 10 should correspond to, you really need to know A to do B, and 0 should mean that they're sort of linked?
+
+    class Meta:
+        unique_together = ("leads_to", "leads_from")
+
     def __unicode__(self):
         return self.leads_from.name + " leads to " + self.leads_to.name
