ba50155c40d82cdbe6a48751612dfd7ea4e8032b
Jack Kirton <jackkirton@hotmail.co.uk>
2013-07-23 19:57:44 +0100

    User friendly lesson URLs
    Change the lesson URL to name with underscores instead of ID.

--- lessons/urls.py
+++ lessons/urls.py
@@ -3,7 +3,7 @@ from lessons import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name = 'index'),
-    url(r'^(?P<lesson_id>\d+)/$', views.read, name = 'read'),
-    url(r'^(?P<lesson_id>\d+)/test/(?P<question_id>\d+)/$', views.question, name = 'question'),
+    url(r'^(?P<lesson_name>\w+)/$', views.read, name = 'read'),
+    url(r'^(?P<lesson_name>\w+)/test/(?P<question_id>\d+)/$', views.question, name = 'question'),
 
 )
--- lessons/views.py
+++ lessons/views.py
@@ -14,18 +14,18 @@ def index(request):
     })
     return render(request, 'lessons/index.html', context)
 
-def read(request, lesson_id):
+def read(request, lesson_name):
     try:
-        lesson = Lesson.objects.get(pk = lesson_id)
-        example_list = Example.objects.filter(lesson__id = lesson_id)
-        first_question = Question.objects.filter(lesson__id = lesson_id)[0]
+        lesson = Lesson.objects.get(name__iexact = lesson_name.replace("_", " "))
+        example_list = Example.objects.filter(lesson__id = lesson.pk)
+        first_question = Question.objects.filter(lesson__id = lesson.pk)[0]
         next_lessons = LessonFollowsFromLesson.objects.filter(leads_from__name = lesson.name).order_by('-strength')
         context = ({'lesson': lesson,'example_list':example_list,'first_question':first_question,'next_lessons':next_lessons,})
     except Lesson.DoesNotExist:
         raise Http404
     return render(request, 'lessons/read.html', context)
 
-def question(request, lesson_id, question_id):
+def question(request, lesson_name, question_id):
     try:
         question = Question.objects.get(pk = question_id)
         pair = generateQuestion(random(), question.question, question.calculation)
