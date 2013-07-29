77f1aef6786617c79a880252ec7a30bf5c9b6e79
Alex Wendland <a.p.wendland@gmail.com>
2013-07-29 02:19:43 +0100

    Tests impliments
    Adressed #5

--- README.md
+++ README.md
@@ -32,3 +32,55 @@ Once that is correct we need to populate your database with mimirs tables, so ru
 Finally test it out with
 
 `python manage.py runserver`
+
+Testing
+=======
+
+Set-up
+======
+
+If you would like to run the test on mimir some set up is required.
+I will assume you have followed the guide above already.
+
+First of all go to:
+
+https://pypi.python.org/pypi/selenium
+
+and download the selenium package to run a dumby server for testing. Next install this by going to 
+command line finding the file you downloaded it to and running
+
+'python setup.py install'
+
+Next if you haven't already got google Chrome, get it and come into the future.
+
+Now got to:
+
+https://code.google.com/p/chromedriver/downloads/list
+
+download the correct chromedriver for your computer. Here lots of different OS vary slight in the set up
+of this. If your a windows guy, you need to put the chromedriver.exe in
+
+<your python directory>\Scripts
+
+if not, I am sorry bud your own your own, lots of advice about this on stack over flow.
+
+Using
+=====
+
+So you now should be good to go on testing. There is a couple of test commands (from the mimir directory),
+most basic is
+
+'python manage.py test'
+
+it will run all the test we have written and alot of inbuilt ones in django, for the specific ones we 
+have written use
+
+'python manage.py test lessons.MySeleniumTests.TESTNAME'
+
+with 'TESTNAME' replace by
+
+'test_views' - to test that all the pages accessible load
+'test_login' - to test your able to log in
+'test_register' - to test your able to register
+
+more to follow. Feel free to add to this list if you wish to.
--- lessons/tests.py
+++ lessons/tests.py
@@ -1,16 +1,70 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
-
+from django.core.urlresolvers import reverse
+from django.test import LiveServerTestCase
+from selenium import webdriver
+from selenium.webdriver.chrome.webdriver import WebDriver
 
 class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+    def test_basic_views(self):
+        response = client.get('/')
+        response.status_code
+
+        
+class MySeleniumTests(LiveServerTestCase):
+    fixtures = ['user-data.json']
+
+    @classmethod
+    def setUpClass(cls):
+        cls.selenium = WebDriver()
+        super(MySeleniumTests, cls).setUpClass()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.selenium.quit()
+        super(MySeleniumTests, cls).tearDownClass()
+
+    def test_login(self):
+        self.selenium.get('%s%s' % (self.live_server_url, '/login/'))
+        username_input = self.selenium.find_element_by_name("username")
+        username_input.send_keys('TestUser')
+        password_input = self.selenium.find_element_by_name("password")
+        password_input.send_keys('BlaTestUserBla123')
+        self.selenium.find_element_by_xpath('//input[@value="login"]').click()
+        
+    def test_register(self):
+        self.selenium.get('%s%s' % (self.live_server_url, '/register/'))
+        username_input = self.selenium.find_element_by_name("username")
+        username_input.send_keys('TestUser')
+        password_1_input = self.selenium.find_element_by_name("password1")
+        password_1_input.send_keys('BlaTestUserBla123')
+        password_2_input = self.selenium.find_element_by_name("password2")
+        password_2_input.send_keys('BlaTestUserBla123')
+        self.selenium.find_element_by_xpath('//input[@value="Create the account"]').click()
+        
+    def test_views(self):
+        self.selenium.get('%s%s' % (self.live_server_url, '/register/'))
+        self.selenium.get('%s%s' % (self.live_server_url, '/login/'))
+        self.selenium.get('%s%s' % (self.live_server_url, '/home/'))
+        self.selenium.get('%s%s' % (self.live_server_url, '/lessons/'))
+        self.selenium.get('%s%s' % (self.live_server_url, '/profile/'))
+        self.selenium.get('%s%s' % (self.live_server_url, '/skills/'))
+        self.selenium.get('%s%s' % (self.live_server_url, '/users/'))
+        self.selenium.get('%s%s' % (self.live_server_url, '/register/'))
+    
+    def test_register_login(self):
+        self.selenium.get('%s%s' % (self.live_server_url, '/register/'))
+        username_input = self.selenium.find_element_by_name("username")
+        username_input.send_keys('TestUser')
+        password_1_input = self.selenium.find_element_by_name("password1")
+        password_1_input.send_keys('BlaTestUserBla123')
+        password_2_input = self.selenium.find_element_by_name("password2")
+        password_2_input.send_keys('BlaTestUserBla123')
+        self.selenium.find_element_by_xpath('//input[@value="Create the account"]').click()
+        self.selenium.get('%s%s' % (self.live_server_url, '/logout/'))
+        self.selenium.get('%s%s' % (self.live_server_url, '/login/'))
+        username_input = self.selenium.find_element_by_name("username")
+        username_input.send_keys('TestUser')
+        password_input = self.selenium.find_element_by_name("password")
+        password_input.send_keys('BlaTestUserBla123')
+        self.selenium.find_element_by_xpath('//input[@value="login"]').click()
+        
