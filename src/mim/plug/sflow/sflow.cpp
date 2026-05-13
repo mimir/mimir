992d4b09efac12fdab2a538cc98e9a4f55a29309
Manuel Lata <contact@mlata.me>
2026-05-13 16:53:29 +0200

    [sflow] remove normalizers

--- src/mim/plug/sflow/sflow.cpp
+++ src/mim/plug/sflow/sflow.cpp
@@ -6,6 +6,4 @@
 using namespace mim;
 
 /// Registers normalizers as well as Phase%s and Pass%es for the Axm%s of this Plugin.
-extern "C" MIM_EXPORT Plugin mim_get_plugin() {
-    return {"sflow", MIM_VERSION, plug::sflow::register_normalizers, nullptr, nullptr};
-}
+extern "C" MIM_EXPORT Plugin mim_get_plugin() { return {"sflow", MIM_VERSION, nullptr, nullptr, nullptr}; }
