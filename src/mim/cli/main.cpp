1080fc1474b5a4fe22ba0608ebf2a37f23ed8bf8
Manuel Lata <contact@mlata.me>
2026-05-22 16:55:37 +0200

    [spirv] SpirV -> SPIRV

--- src/mim/cli/main.cpp
+++ src/mim/cli/main.cpp
@@ -20,7 +20,7 @@ using namespace mim;
 using namespace std::literals;
 
 int main(int argc, char** argv) {
-    enum Backends { AST, CFG, Dot, H, PY, LL, SpirV, Md, Mim, Nest, SExpr, SlottedSExpr, Num_Backends };
+    enum Backends { AST, CFG, Dot, H, PY, LL, SPIRV, Md, Mim, Nest, SExpr, SlottedSExpr, Num_Backends };
 
     try {
         Driver driver;
@@ -58,7 +58,7 @@ int main(int argc, char** argv) {
             | lyra::opt(output[H  ],  "file"               )      ["--output-h"             ]("Emits a header file to be used to interface with a plugin in C++.")
             | lyra::opt(output[PY ],  "file"               )      ["--output-py"             ]("Emits a Python enum to be used to interface with a plugin in Python.")
             | lyra::opt(output[LL ],  "file"               )      ["--output-ll"            ]("Compiles the Mim program to LLVM.")
-            | lyra::opt(output[SpirV], "file"              )      ["--output-spirv"         ]("Compiles the Mim program to Spir-V.")
+            | lyra::opt(output[SPIRV], "file"              )      ["--output-spirv"         ]("Compiles the Mim program to Spir-V.")
             | lyra::opt(output[Md ],  "file"               )      ["--output-md"            ]("Emits the input formatted as Markdown.")
             | lyra::opt(output[Mim],  "file"               )["-o"]["--output-mim"           ]("Emits the Mim program again.")
             | lyra::opt(output[Nest], "file"               )      ["--output-nest"          ]("Emits program nesting tree as Dot.")
@@ -147,7 +147,7 @@ int main(int argc, char** argv) {
 
             if (!flags.bootstrap) {
                 plugins.insert(plugins.begin(), "compile"s);
-                if (os[SpirV]) plugins.emplace_back("spirv"s);
+                if (os[SPIRV]) plugins.emplace_back("spirv"s);
                 if (opt >= 2) plugins.emplace_back("opt"s);
             }
 
@@ -213,7 +213,7 @@ int main(int argc, char** argv) {
                 error("couldn't read file '{}'", input);
             }
 
-            if (auto s = os[SpirV]) {
+            if (auto s = os[SPIRV]) {
                 if (auto backend = driver.backend("spirv"))
                     backend(world, *s);
                 else
