c0543d7e2f9333e0d9646cbd829a135cb407eb9a
Roland Leißa <roland.leissa@cs.uni-goettingen.de>
2026-07-10 13:52:10 +0200

    Revert "SEO::lam2phis_: switching to std::map"
    This reverts commit dad10fc5e60b0505a6bb27459ec7e483a38b90ea.

--- include/mim/plug/mem/phase/seo.h
+++ include/mim/plug/mem/phase/seo.h
@@ -105,7 +105,7 @@ private:
 
     Analysis analysis_;
     Lam2Lam lam2lam_;
-    std::map<Lam*, Vector<Phi>, GIDLt<const Def*>> lam2phis_;
+    absl::btree_map<Lam*, Vector<Phi>> lam2phis_;
 };
 
 } // namespace mim::plug::mem::phase
